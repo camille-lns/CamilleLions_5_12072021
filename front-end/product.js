e3b6282eb57815d7b9ad33f421734438c7a3ba3c
Camille Lions <lionscamille@gmail.com>
2021-07-30 17:45:59 +0200

    mise au point du js produit

--- front-end/product.js
+++ front-end/product.js
@@ -1,5 +1,5 @@
 const searchProduct = new URLSearchParams(window.location.search); 
-const productId = searchProduct.get("_id");
+const productId = searchProduct.get('_id');
 
 const newIdUrl = `http://localhost:3000/api/cameras/${productId}`;
 
@@ -19,24 +19,50 @@ fetch(newIdUrl)
 });
 
 function addProductInfo(produit) {
-    const productImage = document.getElementById("productIMG"); 
-    productImage.innerHTML += `<img src="${produit.imageUrl}" class="img-fluid img-thumbnail">`;
+    const productImage = document.getElementById('productIMG'); 
+    productImage.innerHTML += `<img src="${produit.imageUrl}" class="sepia img-fluid img-thumbnail ultralightbrown">`;
 
-    const productName = document.getElementById("productNAME"); 
+    const productName = document.getElementById('productNAME'); 
     productName.innerHTML += `<h2>${produit.name}</h2>`; 
 
-    const productDescr = document.getElementById("productDESCR"); 
+    const productDescr = document.getElementById('productDESCR'); 
     productDescr.innerHTML += `<p class="card-text">${produit.description}</p>`;
 
-    const productPrice = document.getElementById("productPrice"); 
+    const productPrice = document.getElementById('productPrice'); 
     productPrice.innerHTML += `<h4>${produit.price}</h4>`;
 
     chooseLenses(produit);
 }
 
 function chooseLenses(product) {
-    const specLenses = document.getElementById("lentille");
+    const specLenses = document.getElementById('lentille');
     for (let lenses of product.lenses) {
         specLenses.innerHTML += `<option value="${lenses}">${lenses}</option>`;
     }
 }
+
+const btn = document.getElementById('add');
+let selected = [];
+
+for (let i = 0; i < document.querySelectorAll('select').length; i++) {
+    let selector = document.querySelectorAll('select')[i];
+
+    selected[i] = false;
+
+    selector.addEventListener('click', function() {
+        if (selector.value !=='') {
+            selected[i] = true;
+        } else {
+            selected[i] = false; 
+        }
+        if (selected.includes(false)) {
+                btn.disabled = true;
+        } else {
+            btn.disabled = false; 
+        }
+        console.log(selector.value);
+    })
+}
+
+
+
